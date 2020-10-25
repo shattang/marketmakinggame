e5314670870b8a23d25a59db480da9d4f5e9a05e
Sid Hattangadi <siddhartha.h@gmail.com>
2020-10-25 10:39:50 -0400

    WIP

---
+++ Client/Lib/ValidationHelpers.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace MarketMakingGame.Client.Lib
+{
+  static class ValidationHelpers
+  {
+    public static (bool Success, string ErrorMessages) ValidateObject<T>(T obj)
+    {
+      var validationResults = new List<ValidationResult>();
+      var validationContext = new ValidationContext(obj, null, null);
+      var b = Validator.TryValidateObject(obj, validationContext, validationResults, true);
+      return (b, String.Join(Environment.NewLine, validationResults.Select(x => x.ErrorMessage)));
+    }
+
+    public static (bool Success, string ErrorMessages) ValidateProperty<T, P>(T obj, P propValue, string propertyName)
+    {
+      var validationResults = new List<ValidationResult>();
+      var validationContext = new ValidationContext(obj, null, null) { MemberName = propertyName };
+      var b = Validator.TryValidateProperty(propValue, validationContext, validationResults);
+      return (b, String.Join(Environment.NewLine, validationResults.Select(x => x.ErrorMessage)));
+    }
+  }
+}
